83d0dd6aba191015df19789e8fd5f4834a864ce5
OpenGHz <1352674740@qq.com>
2024-03-24 00:13:54 +0800

    1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="std_vision",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     package_dir={"": "."},
     install_requires=["numpy", "matplotlib", "opencv-python"],
